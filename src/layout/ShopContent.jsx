db6d347992806ee2f05f8a128a057d6e31385081
Khoby-790 <ebaidoo79@gmail.com>
2021-02-23 02:55:21 +0000

    --- Auto-Git Commit ---

--- src/layout/ShopContent.jsx
+++ src/layout/ShopContent.jsx
@@ -6,25 +6,21 @@ import FlitersPane from './FlitersPane'
 const ShopContent = () => {
     return (
         <Fragment>
-             <div class="flex-grow w-full lg:flex">
-                    {/* <!-- Left sidebar & main wrapper --> */}
-                    <div class="flex-1 min-w-0 bg-white xl:flex">
-                        <FlitersPane />
-                        <ItemsListing />
-                    </div>
-                    <div class="bg-white  max-w-md w-full lg:flex-shrink-0 lg:border-l lg:border-gray-200 xl:pr-0">
-                        <div class="h-full pl-6 py-6 lg:w-72">
-                            {/* <!-- Start right column area --> */}
-                            <div class="h-full relative" >
-                                <div class="absolute inset-0 rounded-lg">
-                                    <EmptyCartIcon />
-                                </div>
-                            </div>
-                            {/* <!-- End right column area --> */}
-                        </div>
+            <div class="flex-grow w-full lg:flex">
+                {/* <!-- Left sidebar & main wrapper --> */}
+                <div class="flex-1 min-w-0 bg-white xl:flex">
+                    <FlitersPane />
+                    <ItemsListing />
+                </div>
+                <div class="bg-white  max-w-md w-full lg:flex-shrink-0 lg:border-l lg:border-gray-200 xl:pr-0">
+                    <div class="h-full pl-6 py-6 lg:w-72">
+                        {/* <!-- Start right column area --> */}
+                        <EmptyCartIcon />
+                        {/* <!-- End right column area --> */}
                     </div>
                 </div>
-            
+            </div>
+
         </Fragment>
     )
 }
