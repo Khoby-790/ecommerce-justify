abc7c45a05ed81123466555a90eb2ff231d192e6
Khoby-790 <ebaidoo79@gmail.com>
2021-02-23 03:21:39 +0000

    --- Auto-Git Commit ---

--- src/redux/reducers/shopReducer.js
+++ src/redux/reducers/shopReducer.js
@@ -1,5 +1,22 @@
 import sneakers from '../../assets/shops.json';
 
+function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
+}
+
+const allCategories = sneakers.map(el => el.category);
+
+
+let _categories = [];
+for (let i = 0; i < allCategories.length; i++) {
+    const category = allCategories[i];
+    _categories = [..._categories, ...category]
+}
+_categories = _categories.filter(onlyUnique)
+
 const initialState = {
-    sneakers: sneakers
+    sneakers: sneakers,
+    selectedSneaker: null,
+    categories: _categories,
+    selectedCategories: []
 };
