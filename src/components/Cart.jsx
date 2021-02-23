df2d0d3c89fdb620ed6053dd8b22b1827ddd403e
Khoby-790 <ebaidoo79@gmail.com>
2021-02-23 03:58:45 +0000

    --- Auto-Git Commit ---

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,9 @@
 import React, { Fragment } from 'react'
+import { useSelector } from 'react-redux'
 import EmptyCartIcon from './EmptyCartIcon'
 
 const Cart = () => {
+    const item = useSelector(state => state.selectedSneaker)
     return (
         <Fragment>
             <div class="bg-white dark:bg-gray-500  max-w-md w-full lg:flex-shrink-0 lg:border-l dark:border-white lg:border-gray-200 xl:pr-0">
@@ -9,9 +11,9 @@ const Cart = () => {
                     {/* <!-- Start right column area --> */}
                     <div class="h-full relative" >
                         <div class="absolute inset-0 rounded-lg">
-                            <div className="h-60 w-60">
+                            {!item && <div className="h-60 w-60">
                                 <EmptyCartIcon />
-                            </div>
+                            </div>}
                         </div>
                     </div>
                     {/* <!-- End right column area --> */}
