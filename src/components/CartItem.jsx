1e16cdeb568bdaaff4f47b91ad427255c74c0f15
Khoby-790 <ebaidoo79@gmail.com>
2021-02-23 13:00:46 +0000

    --- Auto-Git Commit ---

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -38,7 +38,7 @@ const CartItem = ({ cartItem, index }) => {
                 </div>
                 <div className="flex-1">
                     <h3 className="font-bold">{cartItem.name}</h3>
-                    <p className="dark:text-gray-400">{cartItem?.designer}</p>
+                    <p className="dark:text-gray-400">{cartItem?.designer || "Community"}</p>
                     <div className="flex mt-4 pr-4 justify-between">
                         <div className="flex">
                             <div onClick={increaseQty} className="border cursor-pointer flex items-center justify-center border-gray-800 dark:border-white rounded-l-lg max-h-6 w-11 h-full px-2">
