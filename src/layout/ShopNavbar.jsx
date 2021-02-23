fe99167aa2182479612d5cbbd43ed5c23f1453e6
Khoby-790 <ebaidoo79@gmail.com>
2021-02-23 04:53:24 +0000

    --- Auto-Git Commit ---

--- src/layout/ShopNavbar.jsx
+++ src/layout/ShopNavbar.jsx
@@ -52,10 +52,14 @@ const ShopNavbar = () => {
                         <div class="hidden lg:block lg:w-80">
                             <div class="flex items-center justify-end">
                                 <div class="flex">
-
+                                    <div className="">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+                                        </svg>
+                                    </div>
                                 </div>
                                 {/* <!-- Profile dropdown --> */}
-                                <div class="ml-4 relative  flex flex-shrink-0">
+                                <div class="ml-4 relative flex-shrink-0">
                                     <div onClick={() => setShowProfileMenu(c => !c)}>
                                         <button class="bg-yellow-600 dark:bg-green-400 flex text-sm rounded-full text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-yellow-400 focus:ring-white" id="user-menu" aria-haspopup="true">
                                             <span class="sr-only">Open user menu</span>
@@ -82,11 +86,7 @@ const ShopNavbar = () => {
                                         </div>
                                     </Transition>
 
-                                    <div className="">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
-                                        </svg>
-                                    </div>
+
                                 </div>
                             </div>
                         </div>
