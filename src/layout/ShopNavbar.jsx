8ae08a83e6e3379b4fc46869daa15f19fffac644
Khoby-790 <ebaidoo79@gmail.com>
2021-02-23 04:59:57 +0000

    --- Auto-Git Commit ---

--- src/layout/ShopNavbar.jsx
+++ src/layout/ShopNavbar.jsx
@@ -84,7 +84,7 @@ const ShopNavbar = () => {
 
 
                                 </div>
-                                <div className="relative h-11 w-11 ml-3 rounded-full bg-gray-100">
+                                <div className="relative bg-gray-900 h-13 w-13 cursor-pointer flex justify-center items-center ml-3 rounded-full bg-gray-100">
                                     <svg className="h-11" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
                                     </svg>
