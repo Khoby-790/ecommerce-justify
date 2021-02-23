f7df95298c5f5b4258311a552cc7bfd73f7f6fdb
Khoby-790 <ebaidoo79@gmail.com>
2021-02-23 04:57:49 +0000

    --- Auto-Git Commit ---

--- src/layout/ShopNavbar.jsx
+++ src/layout/ShopNavbar.jsx
@@ -55,38 +55,11 @@ const ShopNavbar = () => {
 
                                 </div>
                                 {/* <!-- Profile dropdown --> */}
-                                <div class="ml-4 relative flex-shrink-0">
-                                    <div onClick={() => setShowProfileMenu(c => !c)}>
-                                        <button class="bg-yellow-600 dark:bg-green-400 flex text-sm rounded-full text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-yellow-400 focus:ring-white" id="user-menu" aria-haspopup="true">
-                                            <span class="sr-only">Open user menu</span>
-                                            <div className=" h-10 w-10 flex items-center justify-center rounded-full">
-                                                <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=256&h=256&q=80" alt="" />
-                                            </div>
-                                        </button>
-                                    </div>
-
-
-                                    <Transition
-                                        show={showProfileMenu}
-                                        enter="transition ease-out duration-100"
-                                        enterFrom="transform opacity-0 scale-95"
-                                        enterTo="transform opacity-100 scale-95"
-                                        leave="transition ease-in duration-75"
-                                        leaveFrom="transform opacity-100 scale-95"
-                                        leaveTo="transform opacity-0 scale-95"
-                                    >
-                                        <div ref={profileMenuRef} class="origin-top-right absolute z-10 right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
-                                            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">View Profile</a>
-                                            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Settings</a>
-                                            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Logout</a>
-                                        </div>
-                                    </Transition>
-
-                                    <div className="relative">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
-                                        </svg>
-                                    </div>
+                                
+                                <div className="relative h-16 w-16">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+                                    </svg>
                                 </div>
                             </div>
                         </div>
