6e1b88ba0df6c7ba15af26c5d77d18f458da2170
Khoby-790 <ebaidoo79@gmail.com>
2021-02-23 02:35:43 +0000

    --- Auto-Git Commit ---

--- src/layout/FlitersPane.jsx
+++ src/layout/FlitersPane.jsx
@@ -1,5 +1,13 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import FliterItemDropDown from '../components/FliterItemDropDown';
+import sneakers from '../assets/shops.json'
+
+
+function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
+}
+
+const allCategories = sneakers.map(el => el.category);
 
 const shippedFromList = [
     {
@@ -11,6 +19,8 @@ const shippedFromList = [
         value: "Ghana"
     }
 ]
+
+
 
 const categoriesListing = [
     {
@@ -32,6 +42,17 @@ const categoriesListing = [
 ]
 
 const FlitersPane = () => {
+    const [categories, setCategories] = useState([])
+    useEffect(() => {
+        let _categories = [];
+        for (let i = 0; i < allCategories.length; i++) {
+            const category = allCategories[i];
+            _categories = [..._categories, ...category]
+        }
+        _categories = _categories.filter(onlyUnique)
+        setCategories(() => [...categories.map(el => ({ label:el, value:el }))])
+    }, [])
+
     return (
         <Fragment>
             <div class="border-b border-gray-200 xl:border-b-0 xl:flex-shrink-0 xl:w-72 xl:border-r xl:border-gray-200 bg-white">
