91ee3cd5c00bc4a2bb62595735511ec347c79be7
Khoby-790 <ebaidoo79@gmail.com>
2021-02-24 20:20:20 +0000

    --- Auto-Git Commit ---

--- src/layout/Profile.jsx
+++ src/layout/Profile.jsx
@@ -11,6 +11,7 @@ const Profile = () => {
     })
     const [showProfileMenu, setShowProfileMenu] = useState(false);
     const [showLogin, setShowLogin] = useState(false);
+    const [rememberme, setRememberme] = useState(false);
     const [showRegister, setShowRegister] = useState(false);
     const auth = useSelector(state => state.auth)
     return (
@@ -62,10 +63,8 @@ const Profile = () => {
                         <input type="password" placeholder="*****" className="outline-none h-12 px-2 focus:outline-none border border-gray-300" />
                     </div>
                     <div class="flex mb-3 items-center">
-                        {/* <!-- Enabled: "bg-indigo-600", Not Enabled: "bg-gray-200" --> */}
-                        <button type="button" class="bg-gray-200 relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" aria-pressed="false" aria-labelledby="annual-billing-label">
+                        <button type="button" class={`${rememberme ? "bg-indigo-600" : "bg-gray-200"} relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`} aria-pressed="false" aria-labelledby="annual-billing-label">
                             <span class="sr-only">Use setting</span>
-                            {/* <!-- Enabled: "translate-x-5", Not Enabled: "translate-x-0" --> */}
                             <span aria-hidden="true" class="translate-x-0 pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200"></span>
                         </button>
                         <span class="ml-3" id="annual-billing-label">
