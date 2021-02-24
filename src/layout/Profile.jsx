a6da355853f2eb23a27613676a38e2c7c7f30ba7
Khoby-790 <ebaidoo79@gmail.com>
2021-02-24 20:21:26 +0000

    --- Auto-Git Commit ---

--- src/layout/Profile.jsx
+++ src/layout/Profile.jsx
@@ -63,9 +63,9 @@ const Profile = () => {
                         <input type="password" placeholder="*****" className="outline-none h-12 px-2 focus:outline-none border border-gray-300" />
                     </div>
                     <div class="flex mb-3 items-center">
-                        <button type="button" class={`${rememberme ? "bg-indigo-600" : "bg-gray-200"} relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`} aria-pressed="false" aria-labelledby="annual-billing-label">
+                        <button onClick={() => setRememberme(prev => !prev)} type="button" class={`${rememberme ? "bg-indigo-600" : "bg-gray-200"} relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`} aria-pressed="false" aria-labelledby="annual-billing-label">
                             <span class="sr-only">Use setting</span>
-                            <span aria-hidden="true" class="translate-x-0 pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200"></span>
+                            <span aria-hidden="true" class={`${rememberme ? "translate-x-5" : "translate-x-0"} pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200`}></span>
                         </button>
                         <span class="ml-3" id="annual-billing-label">
                             <span class="text-sm text-gray-500">Remember me</span>
