6760b45d2c36468e9cc943fe42fb6ee781c47a92
Khoby-790 <ebaidoo79@gmail.com>
2021-02-24 20:48:45 +0000

    --- Auto-Git Commit ---

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 
 const client = new ApolloClient({
-    uri: 'http://localhost:800',
+    uri: 'http://localhost:8000',
     cache: new InMemoryCache()
 });
 
