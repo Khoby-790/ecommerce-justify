91feec55dd804131f4301fe3a400f5cb9d09c928
Khoby-790 <ebaidoo79@gmail.com>
2021-02-24 20:22:32 +0000

    --- Auto-Git Commit ---

--- src/layout/Profile.jsx
+++ src/layout/Profile.jsx
@@ -63,13 +63,29 @@ const Profile = () => {
                         <input type="password" placeholder="*****" className="outline-none h-12 px-2 focus:outline-none border border-gray-300" />
                     </div>
                     <div class="flex mb-3 items-center">
-                        <button onClick={() => setRememberme(prev => !prev)} type="button" class={`${rememberme ? "bg-indigo-600" : "bg-gray-200"} relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`} aria-pressed="false" aria-labelledby="annual-billing-label">
+                        <button onClick={() => setRememberme(prev => !prev)} type="button" class={`${rememberme ? "bg-green-700" : "bg-gray-200"} relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`} aria-pressed="false" aria-labelledby="annual-billing-label">
                             <span class="sr-only">Use setting</span>
                             <span aria-hidden="true" class={`${rememberme ? "translate-x-5" : "translate-x-0"} pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200`}></span>
                         </button>
                         <span class="ml-3" id="annual-billing-label">
                             <span class="text-sm text-gray-500">Remember me</span>
                         </span>
+                    </div>
+                    <div className="flex flex-col">
+                        <button className="bg-green-700 rounded-md text-white py-3">Sign in</button>
+                    </div>
+                </div>
+            </Modal>
+
+            <Modal show={showLogin} setShow={setShowLogin}>
+                <div className="py-4 px-5 bg-white">
+                    <div className="flex flex-col">
+                        <label htmlFor="">Username</label>
+                        <input type="text" placeholder="email or phone" className="outline-none h-12 px-2 focus:outline-none border border-gray-300" />
+                    </div>
+                    <div className="flex mb-2 flex-col">
+                        <label htmlFor="">Password</label>
+                        <input type="password" placeholder="*****" className="outline-none h-12 px-2 focus:outline-none border border-gray-300" />
                     </div>
                     <div className="flex flex-col">
                         <button className="bg-green-700 rounded-md text-white py-3">Sign in</button>
