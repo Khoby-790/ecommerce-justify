395643d4c9464669975bfe8d0616011c3739d621
Khoby-790 <ebaidoo79@gmail.com>
2021-02-28 14:09:25 +0000

    --- Auto-Git Commit ---

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 
 const client = new ApolloClient({
-    uri: 'http://localhost:8000',
+    uri: 'https://powerful-plains-98910.herokuapp.com/',
     cache: new InMemoryCache()
 });
 
